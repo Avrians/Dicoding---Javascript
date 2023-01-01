a6c43e983fa3588fe9f84073cac609c7b362c3f8
avrians <avriansyahb10@gmail.com>
2023-01-01 19:55:32 +0700

    origin

---
+++ array.js
@@ -0,0 +1,20 @@
+/**
+ * 1. Membuat array
+ */
+const days = ['Minggu', 'Senin', 'Selasa', 'Rabu'];
+console.log(days);
+
+
+/**
+ * 2. Element di dalam array diakses atau diubah melalui key (index).
+ */
+
+
+/**
+ * 3. 
+ */
+
+
+/**
+ * 4. 
+ */
